13a6a0a9b78db9ac6b063c1978a14b3f075c8448
ItaloFerreira21 <duduu21q@gmail.com>
2024-04-19 20:41:50 -0300

    mudança

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,9 @@
-import { Header } from "../components/Header/header"
-
-
-
+import { Header } from "../components/Header/header";
 
 export const Home = () => {
   return (
-
-    <div className=" max-w-[360px] max-h-60" >
+    <div className=" max-w-[360px] max-h-60">
       <Header />
     </div>
-  )
-}
+  );
+};
