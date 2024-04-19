27ae8662d8b970a21eaa3ad0f7650ff9599430d3
ItaloFerreira21 <duduu21q@gmail.com>
2024-04-18 22:36:56 -0300

    fontes estilizidas

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,10 +4,10 @@ export function Header() {
     <div className="flex flex-col space-y-6 px-8 py-16 ">
       <div className=" space-y-6 px-8">
         <img src={logo1} />
-        <h1 className=" pb-2">Gerenciando Sua Clinica</h1>
+        <h1 className=" pb-2 text-base font-bold leading-5 text-blue-950">Gerenciando Sua Clinica</h1>
       </div>
 
-      <div className=" space-y-6 py-11 px-9 pb-2 ">
+      <div className=" space-y-6 py-11 px-9 pb-2 text-base font-bold leading-5">
         <h2>Escolha qual seção deseja iniciar:</h2>
       </div>
       <div className=" space-y-4 px-8 " >
