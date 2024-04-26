adc28b543dc1c857e7bdcae5ed1fddd5e8de99a8
ItaloFerreira21 <duduu21q@gmail.com>
2024-04-26 12:57:33 -0300

    botao de cadastro paciente

--- src/components/pacientes/index.tsx
+++ src/components/pacientes/index.tsx
@@ -1,6 +1,6 @@
 import { Search } from "lucide-react";
 import { PacientesHeader } from "./header";
-import { RegisterNewProfileFooter } from "../generics/FooterNewProfiler";
+import { Link } from "react-router-dom";
 
 export const PacientesPage = () => {
   return (
@@ -17,7 +17,13 @@ export const PacientesPage = () => {
         </div>
       </div>
 
-      <RegisterNewProfileFooter />
+      <footer className="fixed bottom-0  w-full ">
+        <div className="p-4 flex justify-center items-center shadow-2xl">
+          <Link to={"/formulario"} className="bg-primaryBlue hover:bg-primaryBlue-dark text-white font-bold rounded-[6px] focus:outline-none focus:shadow-outline w-[90%] p-4 items-center flex justify-center transform transition-all duration-500 ease-in-out hover:scale-110 hover:brightness-110 hover:animate-pulse ">
+            Cadastrar novo perfil
+          </Link>
+        </div>
+      </footer>
     </div>
   );
 };
