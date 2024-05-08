8f22bf3de9c19826fafc738503a19d955ea2397b
ItaloFerreira21 <duduu21q@gmail.com>
2024-05-08 16:25:34 -0300

    estilizando consultas

--- src/pages/Consultas.tsx
+++ src/pages/Consultas.tsx
@@ -1,10 +1,17 @@
-import { ConsultasInicio } from "@/components/consultas/inicio"
+import { ConsultasBasisContent } from "@/components/consultas/basisContent";
+import { FormConsultas } from "@/components/consultas/form";
+import { BasisPage } from "@/components/generics/basisPage"
+
 
  
 
 
 export const Consultas = () => {
-  return (
-    <ConsultasInicio />
-  )
-}
+    return (
+      <BasisPage
+        basisContent={<ConsultasBasisContent />}
+        headerTitle="Consultas"
+        formComponent={<FormConsultas />}
+      />
+    );
+  };
